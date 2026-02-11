3bbb72232a19f864bf10dd73abc96bc1ead53b04
Riley Horrix <rh1122@ic.ac.uk>
2026-02-11 18:43:03 +0000

    feat: A fresh start

--- test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <catch2/catch_all.hpp>
-
-#include "test/test.h"
-
-using namespace Dae;
-
-void CatchTestListener::testCaseStarting(Catch::TestCaseInfo const& info){
-    testInfo(info.tagsAsString().c_str(), "%s", info.name.c_str());
-}
