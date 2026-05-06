669e2898400ae02d85743bffc46bd5820795dfe2
rileyhorrix <riley@horrix.com>
2026-05-06 07:17:53 +0100

    feat: Changed to attitude control

--- src/envs/neural_plane.py
+++ src/envs/neural_plane.py
@@ -17,7 +17,13 @@ class NeuralPlaneEnv(BaseEnv):
     def __init__(self, config: Config, device: torch.device):
         super().__init__(config, num_envs=config('num_envs', default=1), obs_dim=22, action_dim=4, device=device)
 
-        self.env = ControlEnv(num_envs=self.num_envs, config='tracking', model='F16', device=device)
+        if config('env_type') == 'control':
+            self.env = ControlEnv(num_envs=self.num_envs, config='tracking', model='F16', device=device)
+        elif config('env_type') == 'attitude':
+            self.env = ControlEnv(num_envs=self.num_envs, config='control', model='F16', device=device)
+        else:
+            raise ValueError(f"Unsupported env_type: {config('env_type')}")
+
 
         # Setup environment parameters based on config
         self.env.model.min_altitude = config('min_init_altitude', default=1000.0)
