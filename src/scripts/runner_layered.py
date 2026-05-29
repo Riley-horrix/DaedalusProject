424cb8ea2ce14e4d465865ca1ad7f1da74b18374
rileyhorrix <riley@horrix.com>
2026-05-29 10:34:59 +0100

    fix: Config name

--- src/scripts/runner_layered.py
+++ src/scripts/runner_layered.py
@@ -11,10 +11,8 @@ if __name__ == "__main__":
     agent_config = Config('layered_sac_agent')
     agent_config.load_from_file('src/algorithms/sac/layered_config.json')
 
-    # attitude_config = Config('attitude_agent')
+    attitude_config = Config('attitude_agent')
     # attitude_config.load_from_file('src/algorithms/sac/attitude_config.json')
-
-    attitude_config = Config('td3_agent')
     attitude_config.load_from_file('src/algorithms/td3/td3_config.json')
 
     # Parse command line arguments for logging to wandb
